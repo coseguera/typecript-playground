69384010f0d69a1656f7d02c0df9e31ce9659caf
coseguera <coseguera@users.noreply.github.com>
2023-08-10 22:03:58 +0000

    get results

--- src/sayhello.ts
+++ src/sayhello.ts
@@ -1,3 +1,51 @@
 export function sayHello() {
     return "hello!"
 }
+/**
+ * Process a list of ballots, and return all candidates sorted in descending order by their total number of points.
+List<String> getResults(List<Ballot> ballots)
+A ballot: A, B, C
+A -> 3, B -> 2, C -> 1
+
+
+represent a ballot (up to 3 votes in order)
+map where the key is name and value is count
+
+iterate ballots -> iterate votes
+
+at least one vote at most 3 votes
+
+ */ 
+
+export interface Ballot {
+    votes: string[]
+}
+
+export function getResults(ballots: Ballot[]): string[] {
+    const resultMap = new Map<string, number>();
+    let candidate = '';
+
+    for (let i = 0; i < ballots.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            candidate = ballots[i].votes[j];
+            if(!candidate) {
+                continue;
+            }
+            console.log(candidate);
+            if (resultMap.has(candidate)) {
+                resultMap.set(candidate, resultMap.get(candidate)! + 3 - j);
+            } else {
+                resultMap.set(candidate, 3 - j);
+            }
+        }
+    }
+
+    const result = [...resultMap.entries()].sort((a, b) => b[1] - a[1]);
+    const values = new Array<string>();
+
+    for (let [key, _] of result) {
+        values.push(key);
+    }
+
+    return values; 
+}
--- tests/sayhello.spec.ts
+++ tests/sayhello.spec.ts
@@ -1,10 +1,64 @@
 import { expect } from "chai";
-import { sayHello } from "../src/sayhello";
+import { Ballot, getResults, sayHello } from "../src/sayhello";
 
 describe("sayHello", () => {
     it("says hello", () => {
         const message = sayHello();
 
         expect(message).to.be.equal("hello!");
+    });
+});
+
+describe("getResults", () => {
+    it("returns results", () => {
+        const x: Ballot = {
+            votes: ['a', 'b', 'c']
+        }
+
+        const result = getResults([x]);
+
+        expect(result.length).to.be.equal(3);
+        expect(result[0]).to.be.equal('a');
+        expect(result[1]).to.be.equal('b');
+        expect(result[2]).to.be.equal('c');
+    });
+
+    it("returns results", () => {
+        const x: Ballot = {
+            votes: ['a', 'b', 'c']
+        }
+        const y: Ballot = {
+            votes: ['a', 'b', 'c']
+        }
+        const z: Ballot = {
+            votes: ['a', 'b', 'c']
+        }
+
+
+        const result = getResults([x, y, z]);
+
+        expect(result.length).to.be.equal(3);
+        expect(result[0]).to.be.equal('a');
+        expect(result[1]).to.be.equal('b');
+        expect(result[2]).to.be.equal('c');
+    });
+
+    it("returns results", () => {
+        const x: Ballot = {
+            votes: ['a', 'b', 'c']
+        }
+        const y: Ballot = {
+            votes: ['b']
+        }
+        const z: Ballot = {
+            votes: ['b']
+        }
+
+        const result = getResults([x, y, z]);
+
+        expect(result.length).to.be.equal(3);
+        expect(result[0]).to.be.equal('b');
+        expect(result[1]).to.be.equal('a');
+        expect(result[2]).to.be.equal('c');
     });
 });
